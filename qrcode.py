9078b6fe9df9d5c24574d6bb5cddb9e5223f9136
Danis W <108304436+DanisHome@users.noreply.github.com>
2022-10-30 15:35:04 +0100

    QrCode

---
+++ qrcode.py
@@ -0,0 +1,12 @@
+# Generate
+import pyqrcode
+
+url = pyqrcode.create("https://nohello.net", error='H')
+url.png("audio/qrcode.png", module_color=(0, 255, 0, 255), background=(0, 0, 0, 255), scale=10)
+
+# Decode
+from PIL import Image
+from pyzbar.pyzbar import decode
+
+data = decode(Image.open("audio/qrcode.png"))
+print(data)
